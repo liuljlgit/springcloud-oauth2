153fc3f40a504d3610961abe2537bd998136c66d
liulijun <liulj@huidiancloud.com>
2018-09-28 14:01:42 +0800

    使用ribbon进行服务调用

--- data-server/src/main/java/com/cloud/data/dataserver/controller/DataController.java
+++ data-server/src/main/java/com/cloud/data/dataserver/controller/DataController.java
@@ -1,18 +1,19 @@
 package com.cloud.data.dataserver.controller;
 
-import org.springframework.stereotype.Controller;
+import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Controller
+@RestController
 public class DataController {
 
     @GetMapping("/week")
     public String getTodayWeek(){
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
         Date now = new Date();
-        return sdf.format(now);
+        return JSON.toJSONString(sdf.format(now));
     }
 }
--- ribbon-server/src/main/java/com/cloud/ribbon/ribbonserver/RibbonServerApplication.java
+++ ribbon-server/src/main/java/com/cloud/ribbon/ribbonserver/RibbonServerApplication.java
@@ -2,11 +2,22 @@ package com.cloud.ribbon.ribbonserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RibbonServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonServerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 }
