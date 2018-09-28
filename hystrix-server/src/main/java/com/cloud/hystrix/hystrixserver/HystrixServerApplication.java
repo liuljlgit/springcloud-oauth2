beb4dc70b0970b30d40ea08982af164c9d765815
liulijun <liulj@huidiancloud.com>
2018-09-28 17:29:34 +0800

    加入熔断器机制

--- hystrix-server/src/main/java/com/cloud/hystrix/hystrixserver/HystrixServerApplication.java
+++ hystrix-server/src/main/java/com/cloud/hystrix/hystrixserver/HystrixServerApplication.java
@@ -2,11 +2,25 @@ package com.cloud.hystrix.hystrixserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class HystrixServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixServerApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
