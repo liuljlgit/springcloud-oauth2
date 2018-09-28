ee5d06a03970971fe35b29d08c380d373ed217ad
liulijun <liulj@huidiancloud.com>
2018-09-28 17:07:47 +0800

    增加熔断仪表盘

---
+++ hystrixdashboard-server/src/main/java/com/cloud/hystrixdashboard/hystrixdashboardserver/HystrixdashboardServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.hystrixdashboard.hystrixdashboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixdashboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixdashboardServerApplication.class, args);
+	}
+}
