8330dc028692ebc521958d7e910414a04b1ce81c
liulijun <liulj@huidiancloud.com>
2018-09-29 15:23:38 +0800

    加入lcn构建分布式事务

---
+++ lcn-server/src/main/java/com/cloud/lcn/lcnserver/LcnServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.lcn.lcnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcnServerApplication.class, args);
+	}
+}
