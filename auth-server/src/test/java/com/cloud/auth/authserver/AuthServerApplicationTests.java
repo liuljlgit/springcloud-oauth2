dcc7960c2c430d02fa2ac72b3ef1bda56714a69c
liulijun <liulj@huidiancloud.com>
2018-12-11 16:04:50 +0800

    引入代码生成包来生成代码信息

--- auth-server/src/test/java/com/cloud/auth/authserver/AuthServerApplicationTests.java
+++ auth-server/src/test/java/com/cloud/auth/authserver/AuthServerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cloud.auth.authserver;
 
+import com.gen.autocode.GenMain;
+import com.gen.autocode.GenProp.GenProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class AuthServerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void autocode() {
+		//数据库配置
+		GenProperties.URL = "jdbc:mysql://192.168.1.139:3306/test";
+		GenProperties.NAME = "root";
+		GenProperties.PASS = "root";
+		GenProperties.DRIVER = "com.mysql.jdbc.Driver";
+		//表配置:可配置表或者视图（视图仅用来查询,视图不生成缓存，没有主键，只生成查询方法）
+		GenProperties.tablenames = "daily_amount,load_time";
+		GenProperties.useCache = Boolean.TRUE;
+		//包路径配置
+		GenProperties.entityPackageOutPath = "com.gen.autocodecall.test.entity";
+		GenProperties.daoPackageOutPath = "com.gen.autocodecall.test.dao";
+		GenProperties.servicePackageOutPath = "com.gen.autocodecall.test.service";
+		GenProperties.redisPackageOutPath = "com.gen.autocodecall.test.cache";
+		GenProperties.controllerPackageOutPath = "com.gen.autocodecall.test.controller";
+		GenProperties.respPackageOutPath = "com.gen.autocodecall.test.webentity";
+		GenProperties.xmlPackageOutPath = "mybatis.mapper.test";
+		//有一下的模板可供选择。cache_template_v1、cache_template_v2、no_cache_template、sub_table_template、view_template
+		GenProperties.templatePath = "template/cache_template_v2";
+		//运行代码生成
+		GenMain.main(null);
 	}
 
 }
