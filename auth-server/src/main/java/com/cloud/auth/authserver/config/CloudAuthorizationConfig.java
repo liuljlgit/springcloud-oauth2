920c5f686745a11695c4a68261e03f0443bf9304
liulijun <liulj@huidiancloud.com>
2018-12-21 15:09:43 +0800

    oauth2动手v1版本

--- auth-server/src/main/java/com/cloud/auth/authserver/config/CloudAuthorizationConfig.java
+++ auth-server/src/main/java/com/cloud/auth/authserver/config/CloudAuthorizationConfig.java
@@ -1,16 +1,15 @@
 package com.cloud.auth.authserver.config;
 
-import com.cloud.auth.authserver.service.inft.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
@@ -21,53 +20,56 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 public class CloudAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    AuthenticationManager authenticationManager;
 
     @Autowired
-    private TokenStore tokenStore;
-
-    @Autowired
-    private ClientDetailsService clientDetailsService;
-
-    @Autowired
-    @Qualifier("cloudTokenServices")
-    private DefaultTokenServices tokenServices;
+    TokenStore tokenStore;
 
     /**
-     * 配置client
+     * 配置客户端信息
      * @param clients
      * @throws Exception
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("cloud")
-                .secret("123456")
-                .accessTokenValiditySeconds(86400) //一天的过期时间
-                .refreshTokenValiditySeconds(100000)
-                .authorizedGrantTypes("refresh_token","password","mobile")
-                .scopes("all");
+        String finalSecret = "{bcrypt}" + new BCryptPasswordEncoder().encode("123456");
+
+        // 配置两个客户端，一个用于password认证一个用于client认证
+        clients.inMemory().withClient("client_1")
+                .resourceIds("client_1")
+                .authorizedGrantTypes("client_credentials", "refresh_token")
+                .scopes("select")
+                .authorities("oauth2")
+                .secret(finalSecret)
+                .and().withClient("client_2")
+                .resourceIds("client_2")
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("server")
+                .authorities("oauth2")
+                .secret(finalSecret);
     }
 
     /**
-     * 配置endpoints端点
+     * 配置endpoints端点信息
      * @param endpoints
      * @throws Exception
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
-                .userDetailsService(userDetailsService);
+                .authenticationManager(authenticationManager)
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
     }
 
     /**
-     * 配置endpoints安全约束配置
+     * 配置端点安全约束规则
      * @param security
      * @throws Exception
      */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+        // 允许表单认证
+        security.allowFormAuthenticationForClients();
     }
-
 
 }
