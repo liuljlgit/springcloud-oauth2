5acf7285da31f1e9242a2b03f9602950ec54b87e
liulijun <liulj@huidiancloud.com>
2018-12-21 16:04:23 +0800

    oauth2资源服务器

---
+++ re1-server/src/main/java/com/cloud/resource/re1server/Re1ServerApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.resource.re1server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Re1ServerApplication.class, args);
+	}
+
+}
+
---
+++ re1-server/src/test/java/com/cloud/resource/re1server/Re1ServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cloud.resource.re1server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Re1ServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
