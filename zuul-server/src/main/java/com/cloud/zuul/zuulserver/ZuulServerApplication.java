ec5d046ef5cc1147abae8aa00fb2dc8c4364545e
liulijun <liulj@huidiancloud.com>
2018-12-25 14:11:46 +0800

    网关代理zuul-server服务

--- zuul-server/src/main/java/com/cloud/zuul/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/cloud/zuul/zuulserver/ZuulServerApplication.java
@@ -1,13 +1,13 @@
-package com.cloud.gateway.gatewayserver;
+package com.cloud.zuul.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayServerApplication {
+public class ZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
 }
--- zuul-server/src/test/java/com/cloud/zuul/zuulserver/ZuulServerApplicationTests.java
+++ zuul-server/src/test/java/com/cloud/zuul/zuulserver/ZuulServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cloud.gateway.gatewayserver;
+package com.cloud.zuul.zuulserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GatewayServerApplicationTests {
+public class ZuulServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
