a1e50b315df549fe3d01d2867b0ba4b5a482c132
liulijun <liulj@huidiancloud.com>
2018-12-25 11:04:04 +0800

    完成资源服务器的资源认证

--- re1-server/src/main/java/com/cloud/resource/re1server/config/ResourceServerConfiguration.java
+++ re1-server/src/main/java/com/cloud/resource/re1server/config/ResourceServerConfiguration.java
@@ -32,7 +32,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/loadTime/**").permitAll()
+                /*.antMatchers("/loadTime/**").permitAll()*/
                 .antMatchers("/**").authenticated(); // 配置访问控制，必须认证后才可以访问
     }
 }
