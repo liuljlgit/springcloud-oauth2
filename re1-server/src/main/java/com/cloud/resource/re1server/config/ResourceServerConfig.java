453dcac8e7d0c69ff3cc5ed9d2ff4139f1bc73f8
liulijun <liulj@huidiancloud.com>
2018-12-25 15:01:07 +0800

    网关代理zuul-server服务

---
+++ re1-server/src/main/java/com/cloud/resource/re1server/config/ResourceServerConfig.java
@@ -0,0 +1,31 @@
+package com.cloud.resource.re1server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${spring.application.name}")
+    private String RESOURCE_ID;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID).stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .exceptionHandling()
+            .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+            .and()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
--- zuul-server/src/main/java/com/cloud/zuul/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/cloud/zuul/zuulserver/ZuulServerApplication.java
@@ -1,9 +1,12 @@
 package com.cloud.zuul.zuulserver;
 
+import com.cloud.zuul.zuulserver.filter.AccessFilter;
+import com.cloud.zuul.zuulserver.filter.CorsZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,5 +17,22 @@ public class ZuulServerApplication {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
+	/**
+	 * 资源过滤器
+	 * @return 资源过滤器
+	 */
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
+	}
+
+	/**
+	 * Cors过滤器
+	 * @return
+	 */
+	@Bean
+	public CorsZuulFilter corsZuulFilter(){
+		return new CorsZuulFilter();
+	}
 }
 
