dd98ad4954bf948728f809727fef2d20b336371f
liulijun <liulj@huidiancloud.com>
2018-12-28 15:35:22 +0800

    配置公钥

--- zuul-server/src/main/java/com/cloud/zuul/zuulserver/config/JwtConfig.java
+++ zuul-server/src/main/java/com/cloud/zuul/zuulserver/config/JwtConfig.java
@@ -1,18 +1,19 @@
 package com.cloud.zuul.zuulserver.config;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
-import java.security.KeyPair;
+import java.io.IOException;
 
 @Configuration
 public class JwtConfig {
@@ -25,13 +26,21 @@ public class JwtConfig {
         return new RedisTokenStore(redisConnectionFactory);
     }
 
+    /**
+     * 配置公钥转换或者可以配置一个跟auth-server一样的JwtAccessTokenConverter
+     * @return
+     */
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        KeyPair keyPair = new KeyStoreKeyFactory(
-                new ClassPathResource("keystore.jks"), "123456".toCharArray())
-                .getKeyPair("o2jks");
-        converter.setKeyPair(keyPair);
+        Resource resource = new ClassPathResource("public.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(resource.getInputStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
         return converter;
     }
 
