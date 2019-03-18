b118498662e78491cf3d6364ce30acbc2ac4a7ca
liulijun <liulj@huidiancloud.com>
2019-03-18 18:05:32 +0800

    在token中加入用户信息

--- auth-server/src/main/java/com/cloud/auth/authserver/security/token/CloudTokenEnhancer.java
+++ auth-server/src/main/java/com/cloud/auth/authserver/security/token/CloudTokenEnhancer.java
@@ -1,5 +1,6 @@
 package com.cloud.auth.authserver.security.token;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -7,13 +8,15 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CloudTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String,Object> addtionalInfo = new HashMap<>();
-        Object principal = authentication.getPrincipal();
-        addtionalInfo.put("principal","test");
+        UserDetails userDetails = (UserDetails)authentication.getPrincipal();
+        addtionalInfo.put("user_name",userDetails.getUsername());
+        addtionalInfo.put("role",userDetails.getAuthorities().stream().map(e->e.getAuthority()).collect(Collectors.toList()));
         //加入自定义信息
         ((DefaultOAuth2AccessToken)accessToken).setAdditionalInformation(addtionalInfo);
         return accessToken;
