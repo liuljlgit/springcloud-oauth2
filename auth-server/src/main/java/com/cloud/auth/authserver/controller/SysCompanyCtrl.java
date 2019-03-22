d27fab25723127276bae09e70dd29a7953dee65c
liulijun <liulj@huidiancloud.com>
2019-03-22 10:34:58 +0800

    Revert "测试git回滚1"
    This reverts commit 50b119e18fa63c6007978c010247826736889564.

--- auth-server/src/main/java/com/cloud/auth/authserver/controller/SysCompanyCtrl.java
+++ auth-server/src/main/java/com/cloud/auth/authserver/controller/SysCompanyCtrl.java
@@ -118,19 +118,6 @@ public class SysCompanyCtrl{
         return RespEntity.ok();
     }
 
-    /**
-     * SysCompany 根据主键删除数据
-     * @return
-     * @throws Exception
-     */
-    @DeleteMapping(value = "/sysCompany/test1/{scId}")
-    public String test1(@PathVariable(value="scId") Long scId) throws  Exception{
-        if(Objects.isNull(scId)){
-            throw new BusiException("入参请求异常") ;
-        }
-        sysCompanyService.deleteSysCompany(scId);
-        return RespEntity.ok();
-    }
 
     /**
      * SysCompany 根据主键删除数据
