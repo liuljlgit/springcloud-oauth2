50766109d00966217abc4cd8f5ba953791b30b08
liulijun <liulj@huidiancloud.com>
2019-03-25 18:24:52 +0800

    Revert "test1"
    This reverts commit 68003bcbf4f4b37d0366bb89a3c7c09dcd1bda39.
    
    # Conflicts:
    #	auth-server/src/main/java/com/cloud/auth/authserver/controller/SysCompanyCtrl.java

--- auth-server/src/main/java/com/cloud/auth/authserver/controller/SysDeptCtrl.java
+++ auth-server/src/main/java/com/cloud/auth/authserver/controller/SysDeptCtrl.java
@@ -1,16 +1,15 @@
 package com.cloud.auth.authserver.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.cloud.auth.authserver.entity.SysDept;
-import com.cloud.auth.authserver.service.inft.ISysDeptService;
-import com.cloud.auth.authserver.webentity.SysDeptResp;
-import com.cloud.common.complexquery.QueryExample;
-import com.cloud.common.exception.BusiException;
-import com.cloud.common.webcomm.RespEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Objects;
+import com.cloud.common.exception.BusiException;
+import com.alibaba.fastjson.JSONObject;
+import com.cloud.common.complexquery.QueryExample;
+import com.cloud.common.webcomm.RespEntity;
+import java.util.*;
+import com.cloud.auth.authserver.service.inft.ISysDeptService;
+import com.cloud.auth.authserver.entity.SysDept;
+import com.cloud.auth.authserver.webentity.SysDeptResp;
 
 /**
  * SysDeptCtrl 控制层方法
@@ -116,10 +115,6 @@ public class SysDeptCtrl{
         }
         sysDeptService.deleteSysDept(sdId);
         return RespEntity.ok();
-    }
-
-    public void test1(){
-
     }
 
 
